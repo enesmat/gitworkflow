73be6daebc732c3aea3749bf4a12d773c84bd6f4
enesmat <enes.matzir.sali@skyered-education.de>
2025-03-07 09:09:43 +0100

    updated echo

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo "🚗 Willkommen in der wilden Github-Autowerkstatt! Dein Rennwagen braucht einen Check! 🏁"
+echo "Willkommen in der Github-Autowerkstatt! Dein Rennwagen braucht einen Check!"
 
 # Die geheime Speed-Dokumentation
 DATEI="rennwagen_geheim_daten.txt"
 
 # Bonus - Überprüfen, ob die geheime Datei existiert
 if [ -f "$DATEI" ]; then
-    echo "✅ Die geheime Rennwagen-Datenbank '$DATEI' wurde gefunden! Der Motor startet auf Turbo! ⚡💥"
+    echo "Die geheime Rennwagen-Datenbank '$DATEI' wurde gefunden! Der Motor startet auf Turbo!"
 else
-    echo "❌ Ups! Die Datei '$DATEI' ist verschwunden! Wurde sie von einem gegnerischen Rennteam gestohlen? 🏎️💨"
-    echo "Keine Sorge, der Wagen läuft auch ohne die Daten, aber leider etwas langsamer 😅"
+    echo "Ups! Die Datei '$DATEI' ist verschwunden! Wurde sie von einem gegnerischen Rennteam gestohlen?"
+    echo "Keine Sorge, der Wagen läuft auch ohne die Daten, aber leider etwas langsamer :)"
 fi
